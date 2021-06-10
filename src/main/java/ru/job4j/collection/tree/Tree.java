094e37589e60901524b33149fc081aa37f354020
Eduard Bucari <eduard.bucari@gmail.com>
2021-06-11 02:28:15 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/main/java/ru/job4j/collection/tree/Tree.java
+++ src/main/java/ru/job4j/collection/tree/Tree.java
@@ -1,33 +1,84 @@
 package ru.job4j.collection.tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Optional;
+import java.util.Queue;
+import java.util.function.Predicate;
 
 /**
- * 1. Для начла создадим интерфейс описывающий наше дерево.
+ * Создать элементарную структуру дерева.
+ * Познакомимся с алгоритмом обхода дерева в ширину - breadth first search.
+ * Задание.
+ * 1. Допишите код в метод add().
+ * 2. Дописать тест на отсутствие child в дереве.
+ *    Если потомок присутствует в дереве, то метод add() должен возвращать false.
+ * 3. Запустите тесты и проверьте работу дерева.
  *
- * Класс Node описывает узел дерева.
- * Узел содержит хранимое значение и ссылки на дочерние узлы.
- *
- * Метод add - Должен находить узел по значению parent и
- * добавлять в него дочерний узел со значением child.
- * В этом методе нужно проверить, что значения child еще нет в дереве а parent есть.
- * Если child есть, то метод должен вернуть false.
- * @param <E> элемент или узел дерева.
+ * Задание:(дополнительное по данному классу)
+ * 1. Добавить метод boolean isBinary().
  */
-public interface Tree<E> {
+  class Tree<E> implements SimpleTree<E> {
+      private final Node<E> root;
 
-    boolean add(E parent, E child);
+      Tree(final E root) {
+          this.root = new Node<>(root);
+      }
 
-    Optional<Node<E>> findBy(E value);
+    /**
+     * Метод add - находит узел по значению parent и добавлять в него дочерний узел со значением child.
+     * В этом методе нужно проверить, что значения child еще нет в дереве а parent есть.
+     * Если child есть, то метод должен вернуть false.
+     *
+     * @param parent Значение узла-родителя к которому добавляем потомка.
+     * @param child  Значение добавляемого потомка.
+     * @return true в случае удачи, иначе false.
+     */
+      @Override
+      public boolean add(E parent, E child) {
+          boolean rsl = false;
+          Optional<Node<E>> foundParent = findBy(parent);
+          if (foundParent.isPresent() && findBy(child).isEmpty()) {
+              rsl = foundParent.get().children.add(new Node<>(child));
+          }
+          return rsl;
+      }
 
-    class Node<E> {
-        final E value;
-        final List<Node<E>> children = new ArrayList<>();
+    /**
+     * Метод findBy(E value) реализует поиск элемента в дереве.
+     *
+     * @param value Значение разыскиваемого элемента.
+     * @return Найденный узел.
+     */
+      @Override
+      public Optional<Node<E>> findBy(E value) {
+          return findNode(el -> el.value.equals(value));
+      }
 
-        public Node(E value) {
-            this.value = value;
-        }
-    }
+    /**
+     * Метод isBinary() проверяет дерево на бинарность (количество потомков >2).
+     *
+     * @return возвращается true, если дерево бинарное, иначе false.
+     */
+      public boolean isBinary() {
+          return findNode(el -> (el.children.size() > 2)).isEmpty();
+      }
+
+    /**
+     * Метод findBy уже реализован и использует алгоритм обхода в ширину.
+     */
+
+      public Optional<Node<E>> findNode(Predicate<Node<E>> condition) {
+          Optional<Node<E>> rsl = Optional.empty();
+          Queue<Node<E>> data = new LinkedList<>();
+          data.offer(this.root);
+          while (!data.isEmpty()) {
+              Node<E> el = data.poll();
+              if (condition.test(el)) {
+                  rsl = Optional.of(el);
+                  break;
+              }
+              data.addAll(el.children);
+          }
+          return rsl;
+      }
 }
--- src/test/java/ru/job4j/collection/tree/TreeTest.java
+++ src/test/java/ru/job4j/collection/tree/TreeTest.java
@@ -4,11 +4,11 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
-public class SimpleTreeTest {
+public class TreeTest {
 
     @Test
     public void when6ElFindLastThen6() {
-        Tree<Integer> tree = new SimpleTree<>(1);
+        Tree<Integer> tree = new Tree<>(1);
         tree.add(1, 2);
         tree.add(1, 3);
         tree.add(1, 4);
@@ -22,7 +22,7 @@ public class SimpleTreeTest {
 
     @Test
     public void when6ElFindNotExitThenOptionEmpty() {
-        Tree<Integer> tree = new SimpleTree<>(1);
+        Tree<Integer> tree = new Tree<>(1);
         tree.add(1, 2);
         assertThat(
                 tree.findBy(7).isPresent(),
@@ -32,15 +32,58 @@ public class SimpleTreeTest {
 
     @Test
     public void whenDuplicate() {
-        Tree<Integer> tree = new SimpleTree<>(1);
+        Tree<Integer> tree = new Tree<>(1);
         tree.add(1, 2);
         tree.add(1, 3);
         assertThat(tree.add(3, 2), is(false));
     }
 
     @Test
-    public void whenNoParent() {
-        Tree<Integer> tree = new SimpleTree<>(1);
+    public void whenNotParent() {
+        Tree<Integer> tree = new Tree<>(1);
         assertThat(tree.add(3, 2), is(false));
     }
+
+    @Test
+    public void whenBinary() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 4);
+        tree.add(2, 5);
+        tree.add(3, 6);
+        tree.add(3, 7);
+        assertThat(tree.isBinary(), is(true));
+    }
+
+    @Test
+    public void whenNotBinary() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        assertThat(tree.isBinary(), is(false));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

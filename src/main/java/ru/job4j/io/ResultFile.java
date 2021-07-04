6e4a7d1a9a698bc277894630b656e4344afd1795
Eduard Bucari <eduard.bucari@gmail.com>
2021-07-05 00:53:35 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+/**
+ * FileOutputStream.
+ * Расчет и побитовый вывод таблицы умножения в файл.
+ */
+public class ResultFile {
+
+    /**
+     * Метод multiple возвращает таблицу умножения и выводит файл.
+     * @param size - размер таблицы.
+     * @return таблица.
+     */
+    public int[][] multiple(int size) {
+
+      int[][] table = new int[size][size];
+      try (FileOutputStream out = new FileOutputStream("../chapter_002/data/multiple.txt")) {
+          for (int i = 0; i < size; i++) {
+              out.write("\n".getBytes());
+              for (int j = 0; j < size; j++) {
+                  table[j][i] = (j + 1) * (i + 1);
+                  String s = String.valueOf((j + 1) * (i + 1));
+                  out.write(s.getBytes());
+              }
+          }
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      return table;
+    }
+}

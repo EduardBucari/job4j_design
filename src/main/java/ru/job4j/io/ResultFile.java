e9df371a4ed9364ded5298437dc0b76151cd1579
Eduard Bucari <eduard.bucari@gmail.com>
2021-07-06 01:29:09 +0300

    0.1. FileOutputStream. [#252488](2)

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -10,6 +10,12 @@ public class ResultFile {
 
     /**
      * Метод multiple возвращает таблицу умножения и выводит файл.
+     *
+     * LineSeparator () — это встроенный метод в Java,
+     * который возвращает системно-зависимую строку разделителя строк.
+     * Он всегда возвращает одно и то же значение — начальное значение
+     * системного свойства line.separator.
+     *
      * @param size - размер таблицы.
      * @return таблица.
      */
@@ -18,7 +24,7 @@ public class ResultFile {
       int[][] table = new int[size][size];
       try (FileOutputStream out = new FileOutputStream("../chapter_002/data/multiple.txt")) {
           for (int i = 0; i < size; i++) {
-              out.write("\n".getBytes());
+              out.write(System.lineSeparator().getBytes());
               for (int j = 0; j < size; j++) {
                   table[j][i] = (j + 1) * (i + 1);
                   String s = String.valueOf((j + 1) * (i + 1));

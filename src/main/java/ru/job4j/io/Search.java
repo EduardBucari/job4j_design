55a37a7e823581147712a470be48346ee17139d7
Eduard Bucari <eduard.bucari@gmail.com>
2021-07-14 02:00:48 +0300

    4.1. Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,49 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import java.util.function.Predicate;
+
+/**
+ * Сканирование файловой системы.
+ * Разрабатываются программы, которые выводят содержимое всей директории включая вложенные директории.
+ * Здесь используется класс Path, а не File. Path - это усовершенствованная модель File.
+ * Задание.
+ * 1. Разработайте программу Search, которая будет искать файлы только по определенному предикату.
+ * 2. Программа должна вернуть файлы с расширением js.
+ *
+ * (Для выполнения задания необходимо создать специальный класс SearchFiles,
+ * c логикой поиска файлов по определённому предикату.)
+ */
+public class Search {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0 || args.length == 1) {
+            throw new IllegalArgumentException(
+                    "Enter path for searching and extension of file: /some/path extension"
+            );
+        }
+        Path start = Paths.get(args[0]);
+        System.out.println(search(start, p -> p
+                .toFile()
+                .getName()
+                .endsWith(args[1])));
+    }
+
+    /**
+     * Метод search - это метод поиска файлов в заданной директории, согласно предикату.
+     *
+     * @param root      Корневая директория.
+     * @param condition Предикат поиска файлов.
+     * @return Список найденных файлов.
+     * @throws IOException При возникновении IO исключений.
+     */
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}

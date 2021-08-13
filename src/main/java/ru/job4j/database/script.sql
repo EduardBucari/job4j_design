5c0e2e069672b8c1f4f9983485e41bd6000c9373
Eduard Bucari <eduard.bucari@gmail.com>
2021-08-14 00:19:32 +0300

    0. Что такое база данных. [#6860]

---
+++ src/main/java/ru/job4j/database/script.sql
@@ -0,0 +1,10 @@
+create table worker(
+	id serial primary key,
+	name varchar(50),
+	birthday date,
+	married boolean
+);
+insert into worker(name, birthday, married) values('David', '2001.05.06', true);
+update worker set name = 'Alex';
+delete from worker;
+select * from worker;

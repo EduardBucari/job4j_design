d6b18775a71d7ff0300c2b2b725626a9271ef56a
Eduard Bucari <eduard.bucari@gmail.com>
2021-10-08 00:19:11 +0300

    0. Виды сборщиков мусора [#6852](2)

---
+++ src/main/java/ru/job4j/gc/GCTypeDemo2.java
@@ -0,0 +1,28 @@
+package ru.job4j.gc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GCTypeDemo2 {
+    private static Map<String, String> sContainer = new HashMap<>();
+
+    public static void main(String[] args) {
+        System.out.println("Start of program!");
+        String stringWithPrefix = "stringWithPrefix";
+
+        for (int i = 0; i < 3000000; i++) {
+            String newString = stringWithPrefix +i;
+            sContainer.put(newString, newString);
+        }
+        System.out.println("Map size: " + sContainer.size());
+        System.gc();
+
+        for (int i = 0; i < 2000000; i++) {
+            String newString = stringWithPrefix + i;
+            sContainer.remove(newString);
+        }
+
+        System.out.println("Map size: " + sContainer.size());
+        System.out.println("End of program!");
+    }
+}

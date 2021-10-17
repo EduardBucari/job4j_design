d60a5c24faa699b81165b191e7e4abcf26b43a6f
Eduard Bucari <eduard.bucari@gmail.com>
2021-10-17 22:34:25 +0300

    1. Реализация кеша на SoftReference(2)

--- src/main/java/ru/job4j/cache/DirFileCache.java
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -36,7 +36,7 @@ public class DirFileCache extends AbstractCache<String, String> {
     protected String load(String key) {
         String result = null;
         try {
-            result = Files.readString(Paths.get(cachingDir + key));
+            result = Files.readString(Paths.get(cachingDir, key));
         } catch (IOException e) {
             System.out.printf("Ошибка загрузки файла: %s%n", key);
         }

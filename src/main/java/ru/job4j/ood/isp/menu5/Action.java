88784824186348b2c9177ba382ef62909dfd5758
Eduard Bucari <eduard.bucari@gmail.com>
2021-11-18 20:27:33 +0300

    Создать меню.5

---
+++ src/main/java/ru/job4j/ood/isp/menu5/Action.java
@@ -0,0 +1,7 @@
+package ru.job4j.ood.isp.menu5;
+
+import java.io.IOException;
+
+public interface Action {
+    void action() throws IOException;
+}
---
+++ src/main/java/ru/job4j/ood/isp/menu5/Add.java
@@ -0,0 +1,5 @@
+package ru.job4j.ood.isp.menu5;
+
+public interface Add {
+    void add(Task task);
+}
